918d7709040fb350cc46e05f05db6f023bfcb0f2
Nikita Kot <kotn01@outlook.cz>
2018-02-02 22:33:51 +0100

    webm video added

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ const del = require('del');
 const runSequence = require('run-sequence');
 
 // Basic Gulp task syntax
-gulp.task('hello', function() {
+gulp.task('hello', function () {
     console.log('Hello Zell!');
 });
 
@@ -19,7 +19,7 @@ gulp.task('hello', function() {
 // -----------------
 
 // Start browserSync server
-gulp.task('browserSync', function() {
+gulp.task('browserSync', function () {
     browserSync({
         server: {
             baseDir: 'app'
@@ -27,7 +27,7 @@ gulp.task('browserSync', function() {
     })
 });
 
-gulp.task('sass', function() {
+gulp.task('sass', function () {
     return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
         .pipe(sass().on('error', sass.logError)) // Passes it through a gulp-sass, log errors to console
         .pipe(gulp.dest('app/css')) // Outputs it in the css folder
@@ -37,7 +37,7 @@ gulp.task('sass', function() {
 });
 
 // Watchers
-gulp.task('watch', function() {
+gulp.task('watch', function () {
     gulp.watch('app/scss/**/*.scss', ['sass']);
     gulp.watch('app/*.html', browserSync.reload);
     gulp.watch('app/js/**/*.js', browserSync.reload);
@@ -47,7 +47,7 @@ gulp.task('watch', function() {
 // ------------------
 
 // Optimizing CSS and JavaScript
-gulp.task('useref', function() {
+gulp.task('useref', function () {
 
     return gulp.src('app/*.html')
         .pipe(useref())
@@ -57,7 +57,7 @@ gulp.task('useref', function() {
 });
 
 // Optimizing Images
-gulp.task('images', function() {
+gulp.task('images', function () {
     return gulp.src('app/images/**/*.+(png|jpg|jpeg|gif|svg)')
     // Caching images that ran through imagemin
         .pipe(cache(imagemin({
@@ -67,32 +67,39 @@ gulp.task('images', function() {
 });
 
 // Copying fonts
-gulp.task('fonts', function() {
+gulp.task('fonts', function () {
     return gulp.src('app/fonts/**/*')
         .pipe(gulp.dest('dist/fonts'))
 });
 
+// Copyng videos
+
+gulp.task('videos', function () {
+    return gulp.src('app/videos/**/*')
+        .pipe(gulp.dest('dist/videos'));
+});
+
 // Cleaning
-gulp.task('clean', function() {
-    return del.sync('dist').then(function(cb) {
+gulp.task('clean', function () {
+    return del.sync('dist').then(function (cb) {
         return cache.clearAll(cb);
     });
 });
 
-gulp.task('clean:dist', function() {
+gulp.task('clean:dist', function () {
     return del.sync(['dist/**/*', '!dist/images', '!dist/images/**/*']);
 });
 
 // Build Sequences
 // ---------------
 
-gulp.task('default', function(callback) {
+gulp.task('default', function (callback) {
     runSequence(['sass', 'browserSync'], 'watch',
         callback
     )
 });
 
-gulp.task('build', function(callback) {
+gulp.task('build', function (callback) {
     runSequence(
         'clean:dist',
         'sass',
