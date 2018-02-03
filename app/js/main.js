f88c3ccce1618654cd8a13f2f7f8b2ab23f9e279
Nikita Kot <kotn01@outlook.cz>
2018-02-03 14:06:15 +0100

    ios

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,11 @@
+// Detect if device is iOS
+var iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+if (iOS) {
+    $('video').ready(function () {
+        $('video').get(0).play();
+    });
+}
+// background colors hashes
 var colors = ['#1bedff', '#50c878', '#911bff', '#ff80bc', '#ffb21e'];
-
 var index = Math.floor(Math.random() * colors.length);
-
 $("body").css("background-color", colors[index]);
