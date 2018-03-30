adc9c7c41c789380429ddb3384f6e302f72b239b
Nikita Kot <kotn01@outlook.cz>
2018-03-30 19:57:28 +0200

    random videos

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,10 @@
 // background colors hashes
 var colors = ['#1bedff', '#50c878', '#911bff', '#ff80bc', '#ffb21e'];
 var index = Math.floor(Math.random() * colors.length);
-$("body").css("background-color", colors[index]);
+$('body').css('background-color', colors[index]);
 
-// var videoIndex = Math.floor(Math.random() * (max + 1));
+var videoIndex = Math.floor(Math.random() * 22);
+var video_block = $('#video');
+$('#mp4').attr('src', './videos/Videoclip' + videoIndex + '.mp4');
+$('#webm').attr('src', './videos/Videoclip' + videoIndex + '.webm');
+video_block.load();
